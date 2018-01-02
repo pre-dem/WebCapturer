a991ea10d3b0477f24d0dfa716dd6ea764c5d9e9
Siyu Wang <cn.wangsiyu@gmail.com>
2018-01-02 12:44:53 +0800

    update

--- src/chrome/devtools_test.go
+++ src/chrome/devtools_test.go
@@ -4,12 +4,15 @@ import (
 	"testing"
 	"fmt"
 	"io/ioutil"
+	"qiniupkg.com/x/log.v7"
+	"time"
 )
 
 func TestGetScreenShot(t *testing.T) {
 	data, err := GetScreenShot("https://www.taobao.com", "")
-	screenshotName := "screenshot.png"
+	screenshotName := fmt.Sprintf("/Users/wangsiyu/Desktop/screenshot/screen_shot_%s.png", time.Now().Format("2006-01-02 15:04:05"))
 	if err = ioutil.WriteFile(screenshotName, data, 0644); err != nil {
+		log.Error(err)
 		return
 	}
 	fmt.Printf("Saved screenshot: %s\n", screenshotName)
